860165e4f1fb8c05a2f91f951a5de35291cc687f
Vincenzo Ferme <fermevincenzo@gmail.com>
2015-12-27 13:41:58 +0100

    Improved dependency management and testing with a Makefile

--- environment/cpu/src/cloud/benchflow/monitors/CPU.go
+++ environment/cpu/src/cloud/benchflow/monitors/CPU.go
@@ -1,31 +1,31 @@
 package main
- 
+
 import (
-    "fmt"
-    "net/http"
-    "log"
-    "time"
-    "strings"
-    "strconv"
-    "os"
-    "sync"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+	"github.com/fsouza/go-dockerclient"
 )
-import "github.com/fsouza/go-dockerclient"
 
 type Container struct {
-	ID string
+	ID           string
 	statsChannel chan *docker.Stats
-	doneChannel chan bool
-	last5 uint64
-	last5D uint64
-	last5Time time.Time
-	last30 uint64
-	last30D uint64
-	last30Time time.Time
-	last60 uint64
-	last60D uint64
-	last60Time time.Time
-	}
+	doneChannel  chan bool
+	last5        uint64
+	last5D       uint64
+	last5Time    time.Time
+	last30       uint64
+	last30D      uint64
+	last30Time   time.Time
+	last60       uint64
+	last60D      uint64
+	last60Time   time.Time
+}
 
 var containers []Container
 var monitoring bool
@@ -36,19 +36,19 @@ var waitGroup sync.WaitGroup
 func attachToContainer(client docker.Client, container Container) {
 	go func() {
 		err := client.Stats(docker.StatsOptions{
-			ID: container.ID,
-   	 		Stats: container.statsChannel,
-    		Stream: true,
-    		Done: doneChannel,
-    		Timeout: 0,
-			})
+			ID:      container.ID,
+			Stats:   container.statsChannel,
+			Stream:  true,
+			Done:    doneChannel,
+			Timeout: 0,
+		})
 		if err != nil {
-    		log.Fatal(err)
-    		}
-		}()
-	}
+			log.Fatal(err)
+		}
+	}()
+}
 
-func monitorStats(container Container){
+func monitorStats(container Container) {
 	go func() {
 		container.last5 = 0
 		container.last5D = 0
@@ -56,28 +56,28 @@ func monitorStats(container Container){
 		count5 = 0
 		var count5Value uint64
 		count5Value = 0
-		
+
 		container.last30 = 0
 		container.last30D = 0
 		var count30 int
 		count30 = 0
 		var count30Value uint64
 		count30Value = 0
-		
+
 		container.last60 = 0
 		container.last60D = 0
 		var count60 int
 		count60 = 0
 		var count60Value uint64
 		count60Value = 0
-		
+
 		var timestamp time.Time
 		var prev uint64
-		
-		for true{
+
+		for true {
 			select {
-			case <- stopChannel:
-				close(doneChannel);
+			case <-stopChannel:
+				close(doneChannel)
 				waitGroup.Done()
 				return
 			default:
@@ -91,29 +91,29 @@ func monitorStats(container Container){
 				timestamp = stat.Read
 				if count5 == 5 {
 					prev = container.last5
-					container.last5 = count5Value/5
+					container.last5 = count5Value / 5
 					container.last5D = container.last5 - prev
 					container.last5Time = timestamp
 					count5 = 0
-					}
+				}
 				if count30 == 30 {
 					prev = container.last30
-					container.last30 = count30Value/30
+					container.last30 = count30Value / 30
 					container.last30D = container.last30 - prev
 					container.last30Time = timestamp
 					count30 = 0
-					}
+				}
 				if count60 == 60 {
 					prev = container.last60
-					container.last60 = count60Value/60
+					container.last60 = count60Value / 60
 					container.last60D = container.last60 - prev
 					container.last60Time = timestamp
 					count60 = 0
-					}
 				}
 			}
-		}()
-	}
+		}
+	}()
+}
 
 func totalHandler(w http.ResponseWriter, r *http.Request) {
 	if !monitoring {
@@ -124,25 +124,25 @@ func totalHandler(w http.ResponseWriter, r *http.Request) {
 	if query == "all" {
 		for _, each := range containers {
 			if each.ID != "" {
-				fmt.Fprintf(w, each.ID + "\n")
-				fmt.Fprintf(w, "Avg last 5 seconds: " + strconv.FormatUint(each.last5, 10) + "\n")
-				fmt.Fprintf(w, "Avg last 30 seconds: " + strconv.FormatUint(each.last30, 10) + "\n")
-				fmt.Fprintf(w, "Avg last 60 seconds: " + strconv.FormatUint(each.last60, 10) + "\n")
+				fmt.Fprintf(w, each.ID+"\n")
+				fmt.Fprintf(w, "Avg last 5 seconds: "+strconv.FormatUint(each.last5, 10)+"\n")
+				fmt.Fprintf(w, "Avg last 30 seconds: "+strconv.FormatUint(each.last30, 10)+"\n")
+				fmt.Fprintf(w, "Avg last 60 seconds: "+strconv.FormatUint(each.last60, 10)+"\n")
 				fmt.Fprintf(w, "\n")
-				}
 			}
 		}
+	}
 	if query != "all" {
 		for _, each := range containers {
 			if each.ID == query {
-				fmt.Fprintf(w, each.ID + "\n")
-				fmt.Fprintf(w, "Avg last 5 seconds: " + strconv.FormatUint(each.last5, 10) + "\n")
-				fmt.Fprintf(w, "Avg last 30 seconds: " + strconv.FormatUint(each.last30, 10) + "\n")
-				fmt.Fprintf(w, "Avg last 60 seconds: " + strconv.FormatUint(each.last60, 10) + "\n")
+				fmt.Fprintf(w, each.ID+"\n")
+				fmt.Fprintf(w, "Avg last 5 seconds: "+strconv.FormatUint(each.last5, 10)+"\n")
+				fmt.Fprintf(w, "Avg last 30 seconds: "+strconv.FormatUint(each.last30, 10)+"\n")
+				fmt.Fprintf(w, "Avg last 60 seconds: "+strconv.FormatUint(each.last60, 10)+"\n")
 				fmt.Fprintf(w, "\n")
-				}
 			}
 		}
+	}
 }
 
 func deltaHandler(w http.ResponseWriter, r *http.Request) {
@@ -154,25 +154,25 @@ func deltaHandler(w http.ResponseWriter, r *http.Request) {
 	if query == "all" {
 		for _, each := range containers {
 			if each.ID != "" {
-				fmt.Fprintf(w, each.ID + "\n")
-				fmt.Fprintf(w, "Diff last 5 seconds: " + strconv.FormatUint(each.last5D, 10) + "\n")
-				fmt.Fprintf(w, "Diff last 30 seconds: " + strconv.FormatUint(each.last30D, 10) + "\n")
-				fmt.Fprintf(w, "Diff last 60 seconds: " + strconv.FormatUint(each.last60D, 10) + "\n")
+				fmt.Fprintf(w, each.ID+"\n")
+				fmt.Fprintf(w, "Diff last 5 seconds: "+strconv.FormatUint(each.last5D, 10)+"\n")
+				fmt.Fprintf(w, "Diff last 30 seconds: "+strconv.FormatUint(each.last30D, 10)+"\n")
+				fmt.Fprintf(w, "Diff last 60 seconds: "+strconv.FormatUint(each.last60D, 10)+"\n")
 				fmt.Fprintf(w, "\n")
-				}
 			}
 		}
+	}
 	if query != "all" {
 		for _, each := range containers {
 			if each.ID == query {
-				fmt.Fprintf(w, each.ID + "\n")
-				fmt.Fprintf(w, "Diff last 5 seconds: " + strconv.FormatUint(each.last5D, 10) + "\n")
-				fmt.Fprintf(w, "Diff last 30 seconds: " + strconv.FormatUint(each.last30D, 10) + "\n")
-				fmt.Fprintf(w, "Diff last 60 seconds: " + strconv.FormatUint(each.last60D, 10) + "\n")
+				fmt.Fprintf(w, each.ID+"\n")
+				fmt.Fprintf(w, "Diff last 5 seconds: "+strconv.FormatUint(each.last5D, 10)+"\n")
+				fmt.Fprintf(w, "Diff last 30 seconds: "+strconv.FormatUint(each.last30D, 10)+"\n")
+				fmt.Fprintf(w, "Diff last 60 seconds: "+strconv.FormatUint(each.last60D, 10)+"\n")
 				fmt.Fprintf(w, "\n")
-				}
 			}
 		}
+	}
 }
 
 func startMonitoring(w http.ResponseWriter, r *http.Request) {
@@ -213,24 +213,24 @@ func createDockerClient() docker.Client {
 	//path := os.Getenv("DOCKER_CERT_PATH")
 	//endpoint := "tcp://"+os.Getenv("DOCKER_HOST")+":2376"
 	//endpoint = "tcp://192.168.99.100:2376"
-    //ca := fmt.Sprintf("%s/ca.pem", path)
-    //cert := fmt.Sprintf("%s/cert.pem", path)
-    //key := fmt.Sprintf("%s/key.pem", path)
-    //client, err := docker.NewTLSClient(endpoint, cert, key, ca)
+	//ca := fmt.Sprintf("%s/ca.pem", path)
+	//cert := fmt.Sprintf("%s/cert.pem", path)
+	//key := fmt.Sprintf("%s/key.pem", path)
+	//client, err := docker.NewTLSClient(endpoint, cert, key, ca)
 	endpoint := "unix:///var/run/docker.sock"
-    client, err := docker.NewClient(endpoint)
+	client, err := docker.NewClient(endpoint)
 	if err != nil {
 		log.Fatal(err)
-		}
-	return *client
 	}
- 
+	return *client
+}
+
 func main() {
 	monitoring = false
-	
-    http.HandleFunc("/total", totalHandler)
-    http.HandleFunc("/delta", deltaHandler)
-    http.HandleFunc("/start", startMonitoring)
+
+	http.HandleFunc("/total", totalHandler)
+	http.HandleFunc("/delta", deltaHandler)
+	http.HandleFunc("/start", startMonitoring)
 	http.HandleFunc("/stop", stopMonitoring)
-    http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
